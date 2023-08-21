8cfcf0ec6e7e93b64fbead4dce4627335b6cab29
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-08-21 12:10:35 +0200

    Add files via upload

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component ({
+    selector: 'app-board',
+    templateUrl: './app-board.component.html',
+    styleUrls: ['./app-board.component.scss']
+})
+export class BoardComponent  implements OnInit {
+    squares: any[];
+    xIsNext: boolean;
+    winner: string;
+
+    constructor() {}
+
+    ngOnInit() {
+    this.newGame();
+    }
+
+    newGame() {
+        this.squares = Array(9).fill(null);
+        this.winner = null;
+        this.xIsNext = true;
+    }
+
+
+    get player() {
+        return this.xIsNext ? 'X' : 'O';
+
+    }
+
+    makeMove(idx: number) {
+        if (!this.squares[idx]) {
+            this.squares.splice(idx, 1 ,this.player);
+            this.xIsNext = !this.xIsNext;
+        }
+
+        this.winner = this.calculateWinner();
+    }
+
+    calculateWinner() {
+        const line = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 2],
+        ];
+        for ( let i = 0; i < line.length; i++ ) {
+            const [a, b, c] = line[ i ];
+            if (
+                this.squares[a] &&
+                this.squares[a] === this.squares[b] &&
+                this.squares[b] === this.squares[c]
+                ) {
+                    return this.squares[a]
+                }
+    }
+    return null;
+}
+}
